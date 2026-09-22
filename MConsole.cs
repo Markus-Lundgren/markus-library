c2fa22bd00df445ce23583b6500c980fda03d4af
Markus-Lundgren <markus.lundgren.99@gmail.com>
2026-09-22 17:27:10 +0200

    feat: Add method for multi line support

--- MConsole.cs
+++ MConsole.cs
@@ -1,9 +1,18 @@
 static class MConsole
 {
-	static void ClearConsoleLine(int cursorTop)
+	public static void ClearConsoleLine(int cursorTop)
 	{
 		Console.SetCursorPosition(0, cursorTop);
 		Console.Write(new string(' ', Console.WindowWidth));
+		Console.SetCursorPosition(0, cursorTop);
+	}
+	public static void ClearConsoleLineMulti(int cursorTop, int lines)
+	{
+		for (int i = 0; i < lines; i++)
+		{
+			Console.SetCursorPosition(0, cursorTop + i);
+			Console.Write(new string(' ', Console.WindowWidth));
+		}
 		Console.SetCursorPosition(0, cursorTop);
 	}
 }
